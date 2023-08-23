d44e497e2e1d625683e2e629a1f3e8161aaf1817
Akeem Lawrence <akeemwlawrence@gmail.com>
2023-08-23 18:37:07 -0400

    refactored code to hoist function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("The DOM has loaded");
-let cool = document.getElementById("text").textContent = "This is really cool!";
+    cool();
   });
+
+function cool() {
+    let cool = document.getElementById("text").textContent = "This is really cool!";
+}
 
 console.log(
     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
