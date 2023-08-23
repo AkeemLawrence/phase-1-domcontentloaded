ef8f53999da15ba4cb37464ad5a9ad05ed826fdb
Akeem Lawrence <akeemwlawrence@gmail.com>
2023-08-23 17:54:49 -0400

    removed extra confusing code

--- index.js
+++ index.js
@@ -7,13 +7,3 @@ let cool = document.getElementById("text").textContent = "This is really cool!";
 console.log(
     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
   );
-
-console.log(change);
-
-document.addEventListener("DomContentLoaded", function() {
-
-});
-
-console.log(
-    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-  );
